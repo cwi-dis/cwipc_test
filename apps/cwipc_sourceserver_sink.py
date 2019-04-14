5839d027e4f45671a4f9aa11babff76ca8706060
Jack Jansen <Jack.Jansen@cwi.nl>
2019-04-14 23:42:38 +0200

    Optionally limit #pcs transmitted/received, allow grabbing from plyfiles, allow not displaying pc, print statistics at end.

--- apps/cwipc_sourceserver_sink.py
+++ apps/cwipc_sourceserver_sink.py
@@ -3,6 +3,7 @@ import os
 import time
 import socket
 import argparse
+import traceback
 import open3d
 import numpy as np
 import cwipc
@@ -27,9 +28,15 @@ def cwipc_to_o3d(pc):
     return rv
         
 class SinkClient:
-    def __init__(self, hostname='localhost', port=4303):
+    def __init__(self, hostname='localhost', port=4303, count=None, display=False):
         self.hostname = hostname
         self.port = port
+        self.count = count
+        self.display = display
+        self.times_recv = []
+        self.times_decode = []
+        self.times_latency = []
+        self.times_completeloop = []
 
     def read_cpc_from_socket(self):
         with socket.socket() as s:
@@ -37,7 +44,7 @@ class SinkClient:
                 s.connect((self.hostname, self.port))
             except socket.error as err:
                 print('connecting to {}:{}: {}'.format(self.hostname, self.port, err))
-                sys.exit(1)
+                raise
             rv = b''
             while True:
                 data = s.recv(8192)
@@ -47,9 +54,25 @@ class SinkClient:
             
     def receiver_loop(self):
         while True:
+            t0 = time.time()
             cpc = self.read_cpc_from_socket()
+            t1 = time.time()
             pc = self.decompress(cpc)
-            self.show(pc)
+            t2 = time.time()
+            self.times_recv.append(t1-t0)
+            self.times_decode.append(t2-t1)
+            sinkTime = time.time()
+            sourceTime = pc.timestamp() / 1000000
+            self.times_latency.append(sinkTime-sourceTime)
+            if self.display:
+                self.show(pc)
+            pc.free()
+            if self.count != None:
+                self.count -= 1
+                if self.count <= 0:
+                    break
+            t3 = time.time()
+            self.times_completeloop.append(t3-t0)
             
     def decompress(self, cpc):
         decomp = cwipc.codec.cwipc_new_decoder()
@@ -73,14 +96,36 @@ class SinkClient:
 
     def run(self):
         self.receiver_loop()
+
+    def statistics(self):
+        self.print1stat('recv', self.times_recv)
+        self.print1stat('decode', self.times_decode)
+        self.print1stat('latency', self.times_latency)
+        self.print1stat('completeloop', self.times_completeloop)
+        
+    def print1stat(self, name, values):
+        count = len(values)
+        if count == 0:
+            print('{}: count=0'.format(name))
+            return
+        minValue = min(values)
+        maxValue = max(values)
+        avgValue = sum(values) / count
+        print('{}: count={}, average={:.3f}, min={:.3f}, max={:.3f}'.format(name, count, avgValue, minValue, maxValue))
         
 def main():
-    parser = argparse.ArgumentParser(description="Receive compressed pointclouds from a cwipc_sourceserver_source and display them")
+    parser = argparse.ArgumentParser(description="Receive compressed pointclouds from a cwipc_sourceserver_source and optionally display them")
     parser.add_argument("--hostname", action="store", metavar="HOSTNAME", help="Host or IP address to connect to", default="localhost")
     parser.add_argument("--port", type=int, action="store", metavar="PORT", help="Port to connect to", default=4303)
+    parser.add_argument("--count", type=int, action="store", metavar="N", help="Stop receiving after N requests")
+    parser.add_argument("--display", action="store_true", help="Display each pointcloud after it has been received")
     args = parser.parse_args()
-    clt = SinkClient(args.hostname, args.port)
-    clt.run()
+    clt = SinkClient(args.hostname, args.port, args.count, args.display)
+    try:
+        clt.run()
+    except (Exception, KeyboardInterrupt):
+        traceback.print_exc(limit=-1)
+    clt.statistics()
     
 if __name__ == '__main__':
     main()
--- apps/cwipc_sourceserver_source.py
+++ apps/cwipc_sourceserver_source.py
@@ -3,19 +3,62 @@ import os
 import time
 import socket
 import argparse
+import traceback
 import cwipc
 import cwipc.codec
 import cwipc.realsense2
     
+class cwipc_plydir_source:
+    def __init__(self, dirname):
+        self.dirname = dirname
+        self._loadfilenames()
+        
+    def _loadfilenames(self):
+        filenames = os.listdir(self.dirname)
+        filenames = list(filter(lambda fn: fn[-4:] == '.ply', filenames))
+        filenames = sorted(filenames, reverse=True)
+        self.filenames = list(map(lambda fn: os.path.join(self.dirname, fn), filenames))
+        
+    def _nextfilename(self):
+        assert self.filenames
+        rv = self.filenames.pop()
+        if len(self.filenames) == 0:
+            self._loadfilenames()
+        return rv
+            
+    def eof(self):
+        return len(self.filenames) == 0
+        
+    def available(self, wait):
+        return True
+        
+    def get(self):
+        fn = self._nextfilename()
+        pc = cwipc.cwipc_read(fn, int(time.time()*1000000))
+        return pc
+        
+    def free(self):
+        self.filenames = []
+        
 class SourceServer:
-    def __init__(self, port=4303):
+    def __init__(self, port=4303, count=None, plydir=None):
         self.socket = socket.socket()
         self.socket.bind(('', port))
         self.socket.listen()
-        self.grabber = cwipc.realsense2.cwipc_realsense2()
+        if plydir:
+            self.grabber = cwipc_plydir_source(plydir)
+        else:
+            self.grabber = cwipc.realsense2.cwipc_realsense2()
+        self.times_grab = []
+        self.times_encode = []
+        self.times_send = []
+        self.count = count
         
-    def grab_cpc(self):
+    def grab_pc(self):
         pc = self.grabber.get()
+        return pc
+
+    def encode_pc(self, pc):
         enc = cwipc.codec.cwipc_new_encoder()
         enc.feed(pc)
         gotData = enc.available(True)
@@ -28,16 +71,49 @@ class SourceServer:
     def serve(self):
         while True:
             s, _ = self.socket.accept()
-            data = self.grab_cpc()
+            t0 = time.time()
+            pc = self.grab_pc()
+            t1 = time.time()
+            data = self.encode_pc(pc)
+            t2 = time.time()
             s.sendall(data)
             s.close()
+            t3 = time.time()
+            self.times_grab.append(t1-t0)
+            self.times_encode.append(t2-t1)
+            self.times_send.append(t3-t2)
+            if self.count != None:
+                self.count -= 1
+                if self.count <= 0:
+                    break
+            
+    def statistics(self):
+        self.print1stat('grab', self.times_grab)
+        self.print1stat('encode', self.times_encode)
+        self.print1stat('send', self.times_send)
+        
+    def print1stat(self, name, values):
+        count = len(values)
+        if count == 0:
+            print('{}: count=0'.format(name))
+            return
+        minValue = min(values)
+        maxValue = max(values)
+        avgValue = sum(values) / count
+        print('{}: count={}, average={:.3f}, min={:.3f}, max={:.3f}'.format(name, count, avgValue, minValue, maxValue))
             
 def main():
     parser = argparse.ArgumentParser(description="Start server to send compressed pointclouds to a cwipc_sourceserver_sink")
     parser.add_argument("--port", type=int, action="store", metavar="PORT", help="Port to connect to", default=4303)
+    parser.add_argument("--count", type=int, action="store", metavar="N", help="Stop serving after N requests")
+    parser.add_argument("--plydir", action="store", metavar="DIR", help="Load PLY files from DIR in stead of grabbing them from the camera")
     args = parser.parse_args()
-    srv = SourceServer(args.port)
-    srv.serve()
+    srv = SourceServer(args.port, args.count, args.plydir)
+    try:
+        srv.serve()
+    except (Exception, KeyboardInterrupt):
+        traceback.print_exc(limit=-1)
+    srv.statistics()
     
 if __name__ == '__main__':
     main()
