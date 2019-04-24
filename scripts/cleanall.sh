76ef1d967ba5fc472c13537585f7f6f85ec183fa
Jack Jansen <Jack.Jansen@cwi.nl>
2019-04-24 23:40:24 +0200

    Added an optional branchname argument

--- scripts/cleanall.sh
+++ scripts/cleanall.sh
@@ -2,12 +2,16 @@
 set -x
 dirname=`dirname $0`
 dirname=`cd $dirname/../..; pwd`
+case x$1 in
+x) branchname=master ;;
+*) branchname=$1 ;;
+esac
 cd $dirname
 for i in cwipc_util cwipc_realsense2 cwipc_codec ; do
 (
 	cd $i
 	git fetch
-	git checkout master
+	git checkout $branchname
 	git pull
 	rm -rf build
 	mkdir build
