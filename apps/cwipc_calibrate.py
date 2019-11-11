481cc9d62e9a56afc3065d8403e564144ca5f6d7
Jack Jansen <Jack.Jansen@cwi.nl>
2019-11-11 11:27:29 +0100

    Set decimation to 2, 3 gives funny results

--- apps/cwipc_calibrate.py
+++ apps/cwipc_calibrate.py
@@ -20,7 +20,7 @@ CONFIGFILE="""<?xml version="1.0" ?>
     <CameraConfig>
         <system usb2width="640" usb2height="480" usb2fps="15" usb3width="1280" usb3height="720" usb3fps="30" />
         <postprocessing depthfiltering="1" backgroundremoval="1" greenscreenremoval="1" cloudresolution="0" tiling="0" tilingresolution="0.01" tilingmethod="camera">
-            <depthfilterparameters {distance} decimation_value="3" spatial_iterations="4" spatial_alpha="0.25" spatial_delta="30" spatial_filling="0" temporal_alpha="0.4" temporal_delta="20" temporal_percistency="3" />
+            <depthfilterparameters {distance} decimation_value="2" spatial_iterations="4" spatial_alpha="0.25" spatial_delta="30" spatial_filling="0" temporal_alpha="0.4" temporal_delta="20" temporal_percistency="3" />
         </postprocessing>
         {cameras}
     </CameraConfig>
