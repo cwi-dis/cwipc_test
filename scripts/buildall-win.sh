540818c655c164736f50ff9adb78c2a8c2e8cc2f
Jack Jansen <Jack.Jansen@cwi.nl>
2020-10-06 17:52:54 +0200

    Added --notest, optional building of cwipc_kinect, optional building of specified packages

--- scripts/buildall-win.sh
+++ scripts/buildall-win.sh
@@ -6,7 +6,27 @@ cd $dirname
 mkdir -p installed
 instdir=`cd installed; pwd`
 instdir=`cygpath -w "$instdir"`
-for i in cwipc_util cwipc_realsense2 cwipc_codec ; do
+
+notest=
+case x$1 in
+x--notest)
+	notest="notest"
+	shift
+	;;
+esac
+case x$1 in
+x)
+	all="cwipc_util cwipc_realsense2 cwipc_codec"
+	if [ -d cwipc_kinect ]; then
+		all="$all cwipc_kinect"
+	fi
+	;;
+*)
+	all=$@
+	;;
+esac
+
+for i in $all ; do
 (
 	cd $i
 	git fetch
@@ -15,7 +35,9 @@ for i in cwipc_util cwipc_realsense2 cwipc_codec ; do
 		cd build
 		cmake .. -G "Visual Studio 16 2019" -DCMAKE_INSTALL_PREFIX="$instdir" -DJPEG_Turbo_INCLUDE_DIR="C:/libjpeg-turbo64/include" -DJPEG_Turbo_LIBRARY="C:/libjpeg-turbo64/lib/jpeg.lib"
 		cmake --build . --config Release
-		cmake --build . --config Release --target RUN_TESTS
+		if [ "$notest" != "notest"]; then
+			cmake --build . --config Release --target RUN_TESTS
+		fi
 		cmake --build . --config Release --target INSTALL
 	)	
 )
--- scripts/buildall.sh
+++ scripts/buildall.sh
@@ -4,9 +4,29 @@ set -e
 dirname=`dirname $0`
 dirname=`cd $dirname/../..; pwd`
 cd $dirname
-for i in cwipc_util cwipc_realsense2 cwipc_codec ; do
+notest=
+case x$1 in
+x--notest)
+	notest="notest"
+	shift
+	;;
+esac
+case x$1 in
+x)
+	all="cwipc_util cwipc_realsense2 cwipc_codec"
+	if [ -d cwipc_kinect ]; then
+		all="$all cwipc_kinect"
+	fi
+	;;
+*)
+	all=$@
+	;;
+esac
+	
+for i in $all ; do
 if [ -d $i ]; then
 (
+	echo "** Building $i **"
 	cd $i
 	git fetch
 	git pull
@@ -14,9 +34,13 @@ if [ -d $i ]; then
 		cd build
 		cmake ..
 		make
-		make test
+		if [ "$notest" != "notest"]; then
+			make test
+		fi
 		make install
 	)	
 )
+else
+	echo "** Skipping $i - not found **"
 fi
 done
