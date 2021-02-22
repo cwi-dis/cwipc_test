befcbff539fdf5268bf8384c64344fc060fdab66
Jack Jansen <Jack.Jansen@cwi.nl>
2021-02-22 22:29:07 +0100

    Script to clone all new VRTApp repositories (to replace Testbed)

---
+++ scripts/clone_all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -x
+git clone ssh://git@baltig.viaccess-orca.com:8022/VRT/nativeclient-group/VRTApplication.git
+git clone ssh://git@baltig.viaccess-orca.com:8022/VRT/nativeclient-group/VRTApp-Pilot1.git
+git clone ssh://git@baltig.viaccess-orca.com:8022/VRT/nativeclient-group/VRTApp-Pilot2.git
+git clone ssh://git@baltig.viaccess-orca.com:8022/VRT/nativeclient-group/VRTApp-Pilot3.git
+git clone ssh://git@baltig.viaccess-orca.com:8022/VRT/nativeclient-group/VRTApp-HoloMeet.git
+git clone ssh://git@baltig.viaccess-orca.com:8022/VRT/nativeclient-group/VRTApp-MedicalExamination.git
+git clone ssh://git@baltig.viaccess-orca.com:8022/VRT/nativeclient-group/VRTApp-Museum.git
+git clone ssh://git@baltig.viaccess-orca.com:8022/VRT/nativeclient-group/VRTApp-CWI_cake.git
+git clone ssh://git@baltig.viaccess-orca.com:8022/VRT/nativeclient-group/VRTApp-Theatre.git
