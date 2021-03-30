a9d32bb41d66b9e82839b89688576a62f952b7d0
Jack Jansen <Jack.Jansen@cwi.nl>
2021-03-31 00:10:19 +0200

    Script to help converting a local git repo from VO gitlab to gitlab.com.

---
+++ scripts/togitlabcom.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+case x$1 in
+x)
+	echo usage: $0 newurl
+	echo Shows current remotes for this repo. You are prompted to say whether you want
+	echo to cange origin to vo-gitlab and add newurl as the new origin
+	exit 0
+	;;
+esac
+git remote -v
+echo "Do you want to change origin to vo-gitlab and add a new origin $1 ? Type YES if so - "
+read answer
+if [ x$answer == xYES ]; then
+	set -x
+	git remote rename origin vo-gitlab
+	git fetch
+	git remote add origin $1
+	git fetch
+	git branch --set-upstream master origin/master
+	git remote -v
+	git branch -a
+else
+	exit 1
+fi
