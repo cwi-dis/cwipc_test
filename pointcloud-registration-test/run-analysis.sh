2842c65daab75d914c4960886c3a3d34af991111
Jack Jansen <Jack.Jansen@cwi.nl>
2023-11-17 17:53:32 +0100

    Regenerated plots

---
+++ pointcloud-registration-test/run-analysis.sh
@@ -0,0 +1,7 @@
+ALL_MEASUREMENTS="./genregtest/genregtest003/genregtest003.ply ./genregtest/genregtest03/genregtest03.ply ./vrsmall/jack-sideways/jack-sideways.ply ./vrsmall/jack-forward/jack-forward.ply ./vrsmall/boxes/boxes.ply ./vrbig/jack-sideways/jack-sideways.ply ./vrbig/jack-forward/jack-forward.ply ./vrbig/boxes/boxes.ply"
+
+set -x
+
+for ply in $ALL_MEASUREMENTS; do
+	python ../../cwipc/sandbox/registration_error_compute.py $ply
+done
