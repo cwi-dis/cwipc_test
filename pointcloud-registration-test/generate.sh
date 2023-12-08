03b62b2ef68e3ba72c796df5fdeb651285fcb413
Jack Jansen <Jack.Jansen@cwi.nl>
2023-12-08 12:42:50 +0100

    Added a script to generate the test pointclouds (so we remember how they have been created).

---
+++ pointcloud-registration-test/generate.sh
@@ -0,0 +1,3 @@
+python ../../cwipc/sandbox/genregtest.py --single --distance 0 genregtest/genregtest0/genregtest0.ply
+python ../../cwipc/sandbox/genregtest.py --single --distance 0.03 genregtest/genregtest03/genregtest03.ply
+python ../../cwipc/sandbox/genregtest.py --single --distance 0.003 genregtest/genregtest003/genregtest003.ply
