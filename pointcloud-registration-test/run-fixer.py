5e4be0ac704fdca297651d9dc1e2c3035dc874df
Jack Jansen <Jack.Jansen@cwi.nl>
2023-12-08 15:17:33 +0100

    Also use some loot pointclouds to do the testing.

--- pointcloud-registration-test/run-fixer.py
+++ pointcloud-registration-test/run-fixer.py
@@ -1,4 +1,17 @@
-ALL_MEASUREMENTS="./genregtest/genregtest003/genregtest003.ply ./genregtest/genregtest03/genregtest03.ply ./vrsmall/jack-sideways/jack-sideways.ply ./vrsmall/jack-forward/jack-forward.ply ./vrsmall/boxes/boxes.ply ./vrbig/jack-sideways/jack-sideways.ply ./vrbig/jack-forward/jack-forward.ply ./vrbig/boxes/boxes.ply"
+ALL_MEASUREMENTS="
+./genregtest/lootregtest0/lootregtest0.ply
+./genregtest/lootregtest03/lootregtest03.ply
+./genregtest/lootregtest003/lootregtest003.ply
+./genregtest/genregtest0/genregtest0.ply 
+./genregtest/genregtest03/genregtest03.ply 
+./genregtest/genregtest003/genregtest003.ply 
+./vrsmall/jack-sideways/jack-sideways.ply 
+./vrsmall/jack-forward/jack-forward.ply 
+./vrsmall/boxes/boxes.ply 
+./vrbig/jack-sideways/jack-sideways.ply 
+./vrbig/jack-forward/jack-forward.ply 
+./vrbig/boxes/boxes.ply
+"
 
 set -x
 
