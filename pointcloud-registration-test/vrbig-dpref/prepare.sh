a863bacc12046e663a640a175597fd0dc5bdc0d9
Jack Jansen <Jack.Jansen@cwi.nl>
2023-12-13 17:04:21 +0100

    Ran the algorithms on two of the datasets (vrbig-dpref/boxes and vrbig-dpref/simon-forward).

---
+++ pointcloud-registration-test/vrbig-dpref/prepare.sh
@@ -0,0 +1,5 @@
+for fn in boxes jack-forward jack-sideways simon-forward simon-sideways; do
+	cwipc_grab --playback $fn.ply --filter 'colorize(1,"camera")' $fn/
+	mv $fn/pointcloud-0001.ply $fn/$fn.ply
+done
+
