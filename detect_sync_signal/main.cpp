8bc1f5ce60f06aa5f6ff8ed158d8ff1aa4efe4a2
Thomas Röggla <t.roggla@cwi.nl>
2024-10-07 17:07:59 +0200

    added code for pulse detector

---
+++ detect_sync_signal/main.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+
+unsigned long lastPulse = 0;
+unsigned long currentPulse = 0;
+
+void IRAM_ATTR pulseDetected() {
+  lastPulse = millis();
+}
+
+void setup() {
+  Serial.begin(57600);
+  attachInterrupt(GPIO_NUM_34, pulseDetected, RISING);
+}
+
+void loop() {
+  if (lastPulse != currentPulse) {
+    unsigned long pulseLen = lastPulse - currentPulse;
+    currentPulse = lastPulse;
+
+    Serial.print("Pulse lengtLast h: ");
+    Serial.println(pulseLen);
+  }
+}
