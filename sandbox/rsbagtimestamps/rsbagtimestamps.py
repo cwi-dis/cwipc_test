b463bd9eef42d06934d64eba5e18d84c15b37afe
Jack Jansen <Jack.Jansen@cwi.nl>
2024-11-10 23:27:51 +0200

    Stop streaming when timestamps wrap around

--- sandbox/rsbagtimestamps/rsbagtimestamps.py
+++ sandbox/rsbagtimestamps/rsbagtimestamps.py
@@ -2,6 +2,7 @@ import sys
 import argparse
 from typing import List
 import pyrealsense2 as rs
+import time
 
 def main():
     parser = argparse.ArgumentParser(sys.argv[0], "Print timestamps from recorded realsense bag file")
@@ -14,8 +15,8 @@ def printstamps(filenames : List[str]) -> None:
     config = rs.config()
     for fn in filenames:
         rs.config.enable_device_from_file(config, fn)
-    config.enable_stream(rs.stream.depth, rs.format.z16, 15)
-    config.enable_stream(rs.stream.color, rs.format.rgb8, 15)
+    config.enable_stream(rs.stream.depth, rs.format.z16, 30)
+    config.enable_stream(rs.stream.color, rs.format.rgb8, 30)
     profile = config.resolve(pipeline)
     pipeline.start(config)
     prev_depth_timestamp = 0
@@ -27,7 +28,17 @@ def printstamps(filenames : List[str]) -> None:
         color_frame = frames.get_color_frame()
         depth_timestamp = depth_frame.get_timestamp()
         color_timestamp = color_frame.get_timestamp()
+        if prev_depth_timestamp != 0 and depth_timestamp < prev_depth_timestamp:
+            print(f"Depth recording stopped at {time.ctime(prev_depth_timestamp/1000)}")
+            break
+        if prev_color_timestamp != 0 and color_timestamp < prev_color_timestamp:
+            print(f"Color recording stopped at {time.ctime(prev_color_timestamp/1000)}")
+            break
         print(f"{depth_timestamp-color_timestamp}, {depth_timestamp-prev_depth_timestamp}, {color_timestamp-prev_color_timestamp}")
+        if prev_depth_timestamp == 0:
+            print(f"Depth recording started at {time.ctime(depth_timestamp/1000)}")
+        if prev_color_timestamp == 0:
+            print(f"Color recording started at {time.ctime(color_timestamp/1000)}")
         prev_depth_timestamp = depth_timestamp
         prev_color_timestamp = color_timestamp
 
