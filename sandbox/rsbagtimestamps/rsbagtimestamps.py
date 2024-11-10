6daf187bb33d9924956eb742d5f3fa0fdc74e5e5
Jack Jansen <Jack.Jansen@cwi.nl>
2024-11-11 00:05:17 +0200

    Refactoring to prepare for multiple cameras

--- sandbox/rsbagtimestamps/rsbagtimestamps.py
+++ sandbox/rsbagtimestamps/rsbagtimestamps.py
@@ -1,8 +1,59 @@
 import sys
 import argparse
-from typing import List
+from typing import List, Any, Tuple
 import pyrealsense2 as rs
 import time
+
+class BagPipeline:
+
+    def __init__(self, filename: str):
+        self.filename = filename
+        self.pipeline = rs.pipeline()
+        self.config = rs.config()
+        rs.config.enable_device_from_file(self.config, self.filename)
+        self.config.enable_stream(rs.stream.depth, rs.format.z16, 0)
+        self.config.enable_stream(rs.stream.color, rs.format.rgb8, 0)
+        self.profile = self.config.resolve(self.pipeline)
+        self.pipeline.start(self.config)
+        self.current_depth_timestamp = 0
+        self.current_color_timestamp = 0
+        self.current_depth_duration = 0
+        self.current_color_duration = 0
+        self.current_frames = None
+
+    def nextframe(self, notbefore : int) -> bool:
+        if notbefore == 0 or notbefore >= self.current_depth_timestamp:
+            frames = self.pipeline.wait_for_frames()
+            depth_frame = frames.get_depth_frame()
+            color_frame = frames.get_color_frame()
+            depth_timestamp = int(depth_frame.get_timestamp())
+            color_timestamp = int(color_frame.get_timestamp())
+            if self.current_depth_timestamp != 0 and depth_timestamp < self.current_depth_timestamp:
+                print(f"{self.filename}: Depth recording stopped at {time.ctime(self.current_depth_timestamp/1000)}")
+                return False
+            if self.current_color_timestamp != 0 and color_timestamp < self.current_color_timestamp:
+                print(f"{self.filename}: Color recording stopped at {time.ctime(self.current_color_timestamp/1000)}")
+                return False
+            if self.current_depth_timestamp == 0:
+                print(f"{self.filename}: Depth recording started at {time.ctime(depth_timestamp/1000)}")
+                self.current_depth_timestamp = depth_timestamp
+            if self.current_color_timestamp == 0:
+                print(f"{self.filename}: Color recording started at {time.ctime(color_timestamp/1000)}")
+                self.current_color_timestamp = color_timestamp
+            self.current_depth_duration = depth_timestamp - self.current_depth_timestamp
+            self.current_color_duration = color_timestamp - self.current_color_timestamp
+            self.current_depth_timestamp = depth_timestamp
+            self.current_color_timestamp = color_timestamp
+        return True
+
+    def get_frames(self) -> Any:
+        return self.current_frames
+    
+    def get_timestamps(self) -> Tuple[int, int]:
+        return self.current_depth_timestamp, self.current_color_timestamp
+
+    def get_durations(self) -> Tuple[int, int]:
+        return self.current_depth_duration, self.current_color_duration
 
 def main():
     parser = argparse.ArgumentParser(sys.argv[0], "Print timestamps from recorded realsense bag file")
@@ -11,36 +62,18 @@ def main():
     printstamps([args.bagfile])
 
 def printstamps(filenames : List[str]) -> None:
-    pipeline = rs.pipeline()
-    config = rs.config()
-    for fn in filenames:
-        rs.config.enable_device_from_file(config, fn)
-    config.enable_stream(rs.stream.depth, rs.format.z16, 0)
-    config.enable_stream(rs.stream.color, rs.format.rgb8, 0)
-    profile = config.resolve(pipeline)
-    pipeline.start(config)
-    prev_depth_timestamp = 0
-    prev_color_timestamp = 0
+    bagpipeline = BagPipeline(filenames[0])
     print("rgb_d_offset,d_dur,rgb_dur")
+    prev_timestamp = 0
     while True:
-        frames = pipeline.wait_for_frames()
-        depth_frame = frames.get_depth_frame()
-        color_frame = frames.get_color_frame()
-        depth_timestamp = depth_frame.get_timestamp()
-        color_timestamp = color_frame.get_timestamp()
-        if prev_depth_timestamp != 0 and depth_timestamp < prev_depth_timestamp:
-            print(f"Depth recording stopped at {time.ctime(prev_depth_timestamp/1000)}")
+        ok = bagpipeline.nextframe(prev_timestamp)
+        if not ok:
             break
-        if prev_color_timestamp != 0 and color_timestamp < prev_color_timestamp:
-            print(f"Color recording stopped at {time.ctime(prev_color_timestamp/1000)}")
-            break
-        print(f"{depth_timestamp-color_timestamp}, {depth_timestamp-prev_depth_timestamp}, {color_timestamp-prev_color_timestamp}")
-        if prev_depth_timestamp == 0:
-            print(f"Depth recording started at {time.ctime(depth_timestamp/1000)}")
-        if prev_color_timestamp == 0:
-            print(f"Color recording started at {time.ctime(color_timestamp/1000)}")
-        prev_depth_timestamp = depth_timestamp
-        prev_color_timestamp = color_timestamp
+        frames = bagpipeline.get_frames()
+        depth_timestamp, color_timestamp = bagpipeline.get_timestamps()
+        depth_duration, color_duration = bagpipeline.get_durations()
+        print(f"{depth_timestamp-color_timestamp}, {depth_duration}, {color_duration}")
+        prev_timestamp = depth_timestamp
 
 if __name__ == "__main__":
     main()
