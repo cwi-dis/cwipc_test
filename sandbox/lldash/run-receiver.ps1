3450591ae1d81f1e1db9c7c9bb608d737fdc4ad3
BEELZEBUB\DIS <cwidisgroup@gmail.com>
2025-01-07 00:01:19 +0100

    Adding Powershell scripts for use on Windows. Unfortunately bash scripts won't work, because we need to actibvate the python venv in cwipc, and that doesn't work in bash on windows.

---
+++ sandbox/lldash/run-receiver.ps1
@@ -0,0 +1,9 @@
+$my_path = $MyInvocation.MyCommand.Path
+$my_dir = Split-Path $my_path -Parent
+$cwipc_dir = $my_dir + "\..\..\..\cwipc"
+$lldash_dir = $my_dir + "\..\..\..\lldash\installed"
+$lldash_bin_dir = $lldash_dir + "\bin"
+$Env:PATH = $lldash_bin_dir + ";" + $Env:PATH
+$Env:SIGNALS_SMD_PATH = $lldash_bin_dir
+& $cwipc_dir\scripts\activate.ps1
+cwipc_view --sub http://localhost:9000/jacktest/
